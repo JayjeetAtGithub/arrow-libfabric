25ca99e9a4aefb3d8610cc414cf56afac6d9c21f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-12 10:29:32 -0800

    Add Makefile

---
+++ server.cc
@@ -0,0 +1,7 @@
+#include <rdma/fabric.h>
+#include <rdma/fi_domain.h>
+#include <rdma/fi_endpoint.h>
+#include <rdma/fi_cm.h>
+#include <rdma/fi_tagged.h>
+#include <rdma/fi_rma.h>
+#include <rdma/fi_errno.h>
