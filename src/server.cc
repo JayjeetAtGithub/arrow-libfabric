a0367e1fc9d5fe976a7922445f279a4a50a84145
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-12 10:57:45 -0800

    Add hello world

--- src/server.cc
+++ src/server.cc
@@ -10,4 +10,13 @@
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
+
+    struct fi_info *fi;
+    int err = fi_getinfo(FI_VERSION(1,0), NULL, NULL, 0, NULL, &fi);
+    if (!err) {
+        // traverse the fi_info linked list and print out the names of the providers
+        for (struct fi_info *cur_fi = fi; cur_fi != NULL; cur_fi = cur_fi->next) {
+            std::cout << cur_fi->fabric_attr->prov_name << std::endl;
+        }
+    }
 }
