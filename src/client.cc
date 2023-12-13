163dc7aab792f7e65b02021df7c93862beff80ad
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-12-12 19:48:28 -0800

    add types inside client

--- src/client.cc
+++ src/client.cc
@@ -3,7 +3,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <mercury.h>
-#include "types.h"
+#include <mercury.h>
+#include <mercury_bulk.h>
+#include <mercury_proc_string.h>
+#include <mercury_macros.h>
+
+MERCURY_GEN_PROC(save_in_t,
+    ((hg_string_t)(filename))\
+	((hg_size_t)(size))\
+    ((hg_bulk_t)(bulk_handle)))
+
+MERCURY_GEN_PROC(save_out_t, ((int32_t)(ret)))
 
 typedef struct {
     hg_class_t*   hg_class;
